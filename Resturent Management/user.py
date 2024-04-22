51a567ecb6b00911370f80cc3e6f878259b8e557
rajon-git <rajon.zhsust15@gmail.com>
2024-04-23 00:53:07 +0600

    create restaurent and menu class

--- Resturent Management/user.py
+++ Resturent Management/user.py
@@ -27,17 +27,47 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, phone, email, address):
         super().__init__(name, phone, email, address)
+
+    def add_employee(self,restaurant,employee):
+        restaurant.add_employee(employee)
+    
+    def view_employee(self,restaurant):
+        restaurant.view_employee()
+
+class Restaurant:
+    def __init__(self,name):
+        self.name=name
         self.employees = []  #database
 
-    def add_employee(self,name,phone,email,address,age,designation,salary):
-        employee= Employee(name,phone,email,address,age,designation,salary) #employee classe  ekta object touri holo
+    def add_employee(self,employee):
         self.employees.append(employee)
-        print(f"{name} is added")
-    
+
     def view_employee(self):
         print("Employee Lists:-----")
         for emp in self.employees:
             print(f"Name: {emp.name}, Phone: {emp.phone}, Email: {emp.email}, Address: {emp.address}, Age: {emp.age}, Designation: {emp.designation}, Salary: {emp.salary}")
+
+class Menu:
+    def __init__(self):
+        self.items=[]
+
+    def menu_item(self,item):
+        self.items.append(item)
+
+    def find_item(self,item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+            return None
+        
+    def remove_item(self,item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print("Item Deleted Successfully")
+        else:
+            print("Item not found")
+
 
 ad =Admin("Karim",123444,"karim@gmail.com","Dhaka")
 ad.add_employee("Rajon",3333333,"rajon@gmail.com","dahaka",32,'Manager',20000)
