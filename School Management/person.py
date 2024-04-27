c4eb77bd35ad0cfde0dc68d8c82fa2a8701199b1
rajon-git <rajon.zhsust15@gmail.com>
2024-04-27 18:16:35 +0600

    add new project ride sharing

--- School Management/person.py
+++ School Management/person.py
@@ -1,36 +1,43 @@
 import random
 from school import School
-
 class Person:
-    def __init__(self,name):
-        self.name=name
-
+    def __init__(self, name) -> None:
+        self.name = name
+        
+    
 class Teacher(Person):
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         super().__init__(name)
     def evaluate_exam(self):
-        return random.randint(1,100)
-    
-class Student(Person):
-    def __init__(self, name, classroom):
-        super().__init__(name)
-        self.classroom = classroom
-        self.__id= None
-        self.marks = {}
-        self.subject_garde = {} #{'eng': "A"}
-        self.grade=None
+        return random.randint(50, 100)
 
+class Student(Person):
+    def __init__(self, name, classroom) -> None:
+        super().__init__(name)
+        self.classroom = classroom # classroom object
+        self.__id = None
+        self.marks = {} # {"eng" : 78, "ICT" : 90}
+        self.subject_grade = {} # {"eng" : 'A', 'math' : 'A+'}
+        self.grade = None # final grade
+    
     def calculate_final_grade(self):
         sum = 0
-        for grade in self.subject_garde.values():
-            point = School.grade_to_value(grade)
-            sum +=point
-        gpa = sum / len(self.subject_garde)
-        self.grade = School.value_to_grade(gpa)
-
+        for grade in self.subject_grade.values():
+            point = School.grade_to_value(grade) # 5.00
+            sum += point
+        if sum == 0:
+            gpa = 0.00
+            self.grade = 'F'
+        else:
+            gpa = sum / len(self.subject_grade) # 7/2 = 3.50
+            self.grade = School.value_to_grade(gpa)
+        return f"{self.name} Final Grade : {self.grade} with GPA = {gpa}"
+    # rahim.id == 
+    # rahim.id = 12
     @property
     def id(self):
         return self.__id
     @id.setter
-    def id(self,value):
-        self.__id=value
+    def id(self, value):
+        self.__id = value
+        
